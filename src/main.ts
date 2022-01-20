e5e023087beae5d8af7f46381626a02f3b4ca043
Aleix Jiménez <43652477+aleixjf@users.noreply.github.com>
2022-01-20 15:09:54 +0100

    SSL

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,23 @@ import { AppModule } from './app.module';
 import { TypeOrmExceptionFilter } from './shared/filters/type-orm-exception.filter';
 import { ConfigService } from '@nestjs/config';
 
+import * as fs from 'fs';
+import { HttpsOptions } from '@nestjs/common/interfaces/external/https-options.interface';
+
+const cert = 'config/cert.pem';
+const key = 'config/key.pem';
+const httpsOptions: HttpsOptions = {};
+
+if (fs.existsSync(cert) && fs.existsSync(key)) {
+  httpsOptions.cert = fs.readFileSync(cert);
+  httpsOptions.key = fs.readFileSync(key);
+} else if (process.env.APP_CERT && process.env.APP_KEY) {
+  httpsOptions.cert = fs.readFileSync(process.env.APP_CERT);
+  httpsOptions.key = fs.readFileSync(process.env.APP_KEY);
+}
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, { cors: true, httpsOptions });
   app.setGlobalPrefix('api'); //INFO: This is so that all our routes have the api prefix: https://url/api/controller
   //app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
